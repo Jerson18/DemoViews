0f825001ef31848329e2e904e215f0e845dae5d3
Jerson18 <41449342+Jerson18@users.noreply.github.com>
2018-08-09 17:09:32 -0500

    Actualizacion

---
+++ DemoViews/DemoViews/Colores.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoViews
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Colores : ContentPage
+	{
+		public Colores ()
+		{
+			InitializeComponent ();
+		}
+
+        private string ColorToHex(Color color)
+        {
+            int rojo = (int)(color.R * 255);
+            int verde = (int)(color.G * 255);
+            int azul = (int)(color.B * 255);
+            int alpha = (int)(color.A * 255);
+            return $"#{rojo:X2}{verde:X2}{azul:X2}{alpha:X2}";
+        }
+        void CambiarColor(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            var rojo = sldRojo.Value;
+            var azul = sldAzul.Value;
+            var verde = sldVerde.Value;
+            Color bgColor = new Color(rojo, verde, azul);
+            boxColor.BackgroundColor = bgColor;
+            lbDisplay.Text = ColorToHex(bgColor);
+        }
+	}
+}
---
+++ DemoViews/DemoViews/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoViews
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            timPickInicial.Time = new TimeSpan(17, 0, 0);
+		}
+
+        async void Handle_Navigated(object sender, Xamarin.Forms.WebNavigatedEventArgs e)
+        {
+            await DisplayAlert("Completado",
+                                "Se ha completado la carga de la Página",
+                                "OK");
+        }
+
+    }
+
+}
